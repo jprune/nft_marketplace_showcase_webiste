a0821ea904da5974b7ec165d5fc48c4378860128
Julian <105754345+jprune@users.noreply.github.com>
2022-08-22 23:06:09 +0200

    final touches

--- src/components/SectionWrapper.jsx
+++ src/components/SectionWrapper.jsx
@@ -4,24 +4,36 @@ import assets from '../assets'
 import Button from './Button'
 
 
-const SectionWrapper = ({ title, description, showBtn, mockupImg, banner }) => {
+const SectionWrapper = ({ title, description, showBtn, mockupImg, banner, reverse }) => {
   return (
-    <div className={`min-h-screen ${styles.section} ${styles.bgWhite} ${banner}`}>
-      <div className={`flex items-center ${styles.boxClass}`}>
-        <div className={`${styles.descDiv}`}>
-          <h1 className={`${styles.h1Text}`}>{title}</h1>
-          <p className={`${styles.descriptionText}`}>{description}</p>
+    <div className={`min-h-screen ${styles.section} ${reverse ? styles.bgWhite : styles.bgPrimary} ${banner}`}>
+      <div className={`flex items-center ${reverse ? styles.boxReverseClass : styles.boxClass} w-11/12 sm:w-full minmd:w-3/4`}>
+        <div className={`${styles.descDiv}
+          ${reverse ? " fadeRightMini" : " fadeLeftMini"}
+          ${reverse ? styles.textRight : styles.textLeft}
+        `}>
+          <h1 className={`
+            ${styles.h1Text}
+            ${reverse ? styles.blackText : styles.whiteText}
+          `}>{title}</h1>
+          <p className={`${styles.descriptionText} ${reverse ? styles.blackText : styles.whiteText}`}>{description}</p>
           {showBtn && (
             <Button 
               assetUrl={assets.expo}
-              link="deployed nft marketplace react native"
+              link="https://expo.dev/@jprune1/NFT_Marketplace_reactnative?serviceType=classic&distribution=expo-go"
             />
           )}
         </div>
+
+        <div 
+          className={`flex-1 ${styles.flexCenter} p-8 sm:px-0`}
+        >
+          <img src={mockupImg} alt="mockup" className={`
+            ${styles.sectionImg}
+            ${reverse ? " fadeLeftMini" : " fadeRightMini"}
+          `}/>
+        </div>
       </div>
-      
-     
-      {/* <img src={mockupImg} alt="mockup"/> */}
     </div>
   )
 }
