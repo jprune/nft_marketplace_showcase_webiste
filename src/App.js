006408e2078d63c3e1d3f3d562f40fcc885657cc
Julian <105754345+jprune@users.noreply.github.com>
2022-08-22 14:41:09 +0200

    basic structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { Download, SectionWrapper, Features } from "./components"
+import assets from "./assets"
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <SectionWrapper 
+        title="Your own store of Nifty NFTs. Start selling & growing"
+        description="Buy, store, collect NFTs, exchange & earn crypto. Join 25+ million people using ProNef Marketplace."
+        showBtn
+        mockupImg={assets.homeHero}
+        banner="banner"
+      />
+    </>
   );
 }
 
---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+import Download from "./Download.jsx"
+import Features from "./Features.jsx"
+import SectionWrapper from "./SectionWrapper.jsx"
+import Button from "./Button.jsx"
+
+export { Features, Download, SectionWrapper } 
